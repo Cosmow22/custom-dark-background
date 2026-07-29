0fd53fea6faecff57783e2bd4536d0724927c623
Cosmow22 <contact@cosmow22.fr>
2026-07-29 10:47:57 +0200

    save

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,28 @@
+const hex = new RegExp("^#(?:[0-9a-fA-F]{3,4}){1,2}$");
+const rgb = new RegExp("^rgb\((25[0-5]|2[0-4]\d|1?\d?\d),\s*(25[0-5]|2[0-4]\d|1?\d?\d),\s*(25[0-5]|2[0-4]\d|1?\d?\d)\)$");
+
+
 const btn = document.getElementById("btn");
 const colorInput = document.getElementById("color");
 
 btn.addEventListener("click", () => {
-    console.log(colorInput.value, "is set")
-    browser.storage.local.set({ backgroundColor: colorInput.value });
+    let color = colorInput.value;
+    console.log("the input color is: " + color);
+    if (hex.test(color)|| rgb.test(color)) {
+        console.log("the input format is correct");
+        const error = document.getElementById("error-message");
+        if (error) { error.remove();}
+        browser.storage.local.set({ backgroundColor: colorInput.value });
+    } else {
+        console.log("the input format is incorrect");
+        const error = document.getElementById("error-message");
+        if (!error) {
+            const p = document.createElement("p");
+            p.id = "error-message";
+            p.style.color = "red";
+            p.textContent = "⚠️ Invalid color format.";
+    
+            document.body.appendChild(p);
+         }
+    }
 });
